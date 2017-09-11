9fc3ba0e10e824cc21b914e6eb57dfaae14a7671
api <api>
2017-09-11 16:01:08 +0200

    first commit

---
+++ src/demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author api_2
+ *
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);
+	}
+
+}
